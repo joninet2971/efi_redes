7294363a212f42aa51335410720755057054c1e2
Jon Desplats <jon.desplats@gmail.com>
2025-10-03 20:34:09 -0300

    update

--- red_gps/red_gps/settings.py
+++ red_gps/red_gps/settings.py
@@ -103,9 +103,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
+USE_TZ = True
 
 USE_I18N = True
 
